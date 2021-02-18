6c90209ae5487fed00d645f1f55e9c7a331dfbb4
KhengarAbhijeet <abhijit.khengar@iblinfotech.com>
2021-02-18 15:33:35 +0530

    Simple portfolio 1

--- js/new.js
+++ js/new.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.carouselServ,.carouselAbout,.carouselWork').owlCarousel({
       loop:true,
-      margin:10,
+      margin:30,
+      center:true,
+      slideTransition:'linear',
       // nav:true,
       responsive:{
           0:{
               items:1
           },
           600:{
-              items:3
+              items:2
           },
           1000:{
-              items:5
+              items:3
           }
       }
+    });
+
+    $('.ts1').owlCarousel({
+      loop:true,
+      margin:30,
+      items:1,
+      center:true,
+      slideTransition:'linear'
     });
   $('#home').on('activate.bs.scrollspy', function () {
     $('.divhome').removeClass("fadeInUp");
@@ -21,4 +31,8 @@ $(document).ready(function(){
       $('.divhome').addClass("fadeInUp");
     },500);
   });
+
+  $('#aboutme').on('activate.bs.scrollspy',function () {
+    $('.about').addClass("fadeInUp");
+  })
 });
